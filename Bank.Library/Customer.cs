3673da8b1223b797d2c55bba10c6e71a02c59d39
Philippe Berglund <philippe05@gmail.com>
2017-11-01 12:07:52 +0100

    Added customer class

---
+++ Bank.Library/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank.Library
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string OrgNumber { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string AreaCode { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
