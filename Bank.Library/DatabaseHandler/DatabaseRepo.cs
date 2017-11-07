4eae2cf0f7fb136eef10d0c15caa69deda996c17
Mir Albertioni <mir.al@live.se>
2017-11-07 13:30:40 +0100

    databaserepo added with list of customer and account

---
+++ Bank.Library/DatabaseHandler/DatabaseRepo.cs
@@ -0,0 +1,16 @@
+﻿using Bank.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank.Library.DatabaseHandler
+{
+    public class DatabaseRepo
+    {
+        public static HashSet<Customer> Customers { get => _customerList; set => _customerList = value; }
+        public static HashSet<Account> Accounts { get => _accountList; set => _accountList = value; }
+
+        static HashSet<Customer> _customerList = new HashSet<Customer>();
+        static HashSet<Account> _accountList = new HashSet<Account>();
+    }
+}
